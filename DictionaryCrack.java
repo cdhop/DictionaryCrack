185a91afc0d29678a21008ce2df1c3dd243b5084
Christopher Hopkins <cdhop@disquietingcandor.net>
2013-06-05 19:08:55 -0600

    Completed basic functionality and added administrative/build files

--- DictionaryCrack.java
+++ DictionaryCrack.java
@@ -14,16 +14,27 @@ public class DictionaryCrack
 	
 	public String crack(String hash)
 	{
+        String result = null;
+        
 		try
 		{
 			boolean done = false;
 			FileReader fileReader = new FileReader(dictionary);
 			BufferedReader bufferedReader = new BufferedReader(fileReader);
-			String guess;
+			String guess = null;
+            String guess_hash;
 			
 			while(((guess = bufferedReader.readLine()) != null) && !done)
 			{
-			
+                md.reset();
+                md.update(guess.getBytes());
+                guess_hash = hashToString(md.digest());
+                
+                if(guess_hash.equals(hash))
+                {
+                    result = new String(guess);
+                    done = true;
+                }
 			}
 			fileReader.close();
 		}
@@ -32,11 +43,34 @@ public class DictionaryCrack
 			System.out.println("Exception: " + e);
 		}
 		
-		return guess;
+		return result;
 	}
 	
+    protected String hashToString(byte[] hash)
+	{
+		StringBuffer sb = new StringBuffer();
+		
+		for(int i = 0; i < hash.length; i++)
+		{
+			sb.append(Integer.toString((hash[i] & 0xff) + 0x100, 16).substring(1));
+		}
+		return sb.toString();
+	}
+    
 	public static void main(String[] args)
 	{
-	
+        try
+        {
+            if(args.length > 0)
+            {
+                DictionaryCrack dc = new DictionaryCrack();
+                System.out.println("Password is: " + dc.crack(args[0]));
+            }
+        }
+        catch(Exception e)
+        {
+            System.out.println("Exception: " + e);
+        }
+        
 	}
 }
